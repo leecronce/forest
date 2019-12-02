f87c6c773d9a4170a4f9428a960e0c6431a8548a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-12-02 12:30:31 +0000

    add actions to support add/remove layers

--- forest/layers.py
+++ forest/layers.py
@@ -1,13 +1,33 @@
 
 
 SET_VISIBLE = "SET_VISIBLE"
+ADD_LAYER = "ADD_LAYER"
+REMOVE_LAYER = "REMOVE_LAYER"
 
 
 def set_visible(flag):
     return {"kind": SET_VISIBLE, "payload": flag}
 
 
+def add_layer():
+    return {"kind": ADD_LAYER}
+
+
+def remove_layer():
+    return {"kind": REMOVE_LAYER}
+
+
 def reducer(state, action):
-    if action["kind"] == SET_VISIBLE:
+    kind = action["kind"]
+    if kind == SET_VISIBLE:
         return {"visible": action["payload"]}
+    elif kind == ADD_LAYER:
+        return {"layers": [0]}
+    elif kind == REMOVE_LAYER:
+        return {"layers": []}
     return state
+
+
+class View:
+    def render(self, state):
+        pass
--- test/test_layers.py
+++ test/test_layers.py
@@ -4,8 +4,16 @@ from forest import layers
 
 @pytest.mark.parametrize("state,action,expect", [
     ({}, layers.set_visible(True), {"visible": True}),
-    ({}, layers.set_visible(False), {"visible": False})
+    ({}, layers.set_visible(False), {"visible": False}),
+    ({}, layers.add_layer(), {"layers": [0]}),
+    ({"layers": [0]}, layers.remove_layer(), {"layers": []}),
 ])
 def test_reducer(state, action, expect):
     result = layers.reducer(state, action)
     assert expect == result
+
+
+def test_render():
+    state = {}
+    view = layers.View()
+    view.render(state)
