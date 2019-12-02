296146e91147e5990333c50d66c2202048a8711a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-12-02 12:20:59 +0000

    add tests to support set_visible action

--- forest/layers.py
+++ forest/layers.py
@@ -1,4 +1,13 @@
 
 
+SET_VISIBLE = "SET_VISIBLE"
+
+
+def set_visible(flag):
+    return {"kind": SET_VISIBLE, "payload": flag}
+
+
 def reducer(state, action):
+    if action["kind"] == SET_VISIBLE:
+        return {"visible": action["payload"]}
     return state
--- test/test_layers.py
+++ test/test_layers.py
@@ -3,8 +3,9 @@ from forest import layers
 
 
 @pytest.mark.parametrize("state,action,expect", [
-        ({}, {}, {})
-    ])
+    ({}, layers.set_visible(True), {"visible": True}),
+    ({}, layers.set_visible(False), {"visible": False})
+])
 def test_reducer(state, action, expect):
     result = layers.reducer(state, action)
     assert expect == result
